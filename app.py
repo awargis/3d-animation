edfca99448d8e5ac235f93b2d895215db41a732b
awargis <96815206+awargis@users.noreply.github.com>
2026-09-17 09:43:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.sidebar.header("⚙️ Configuration")
 api_key = st.sidebar.text_input("Gemini API Key:", type="password")
 model_name = st.sidebar.selectbox(
     "Gemini model",
-    ["gemini-2.5-flash", "gemini-2.5-pro"],
+    ["gemini-2.5-flash","gemini-3.5-flash-lite", "gemini-3.8-flash","gemini-3.5-flash","gemini-3-flash-preview","gemini-3.1-pro-preview","gemini-2.5-pro"],
     help="Flash = faster/cheaper. Pro = slightly more accurate on dense/cramped pages.",
 )
 quality = st.sidebar.selectbox("Render Quality", ["Low (-ql, Fast)", "Medium (-qm)", "High (-qh, 1080p)"])
